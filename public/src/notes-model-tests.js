cda70c950f2c186a3f6f50034b94ba7989c0e624
hybridbad <hybridbad90@gmail.com>
2019-04-15 15:05:46 +0100

    Add assert file to build testing framework Rewrite model test to include assert function

--- public/src/notes-model-tests.js
+++ public/src/notes-model-tests.js
@@ -1,14 +1,8 @@
-(function(exports) {
-  function testCreateNoteWithText(){
-    var note = new Note('My favourite language is Javascript');
 
-    if(note.text !== 'My favourite language is Javascript'){
-      throw new Error('This text is wrong');
-    } else {
-      console.log('test passed');
-    }
+function testCreateNoteWithText(){
+  var note = new Note('My favourite language is Javascript');
 
-  };
+  assert.isTrue(note.text === "My favourite language is Javascript");
+};
 
-  testCreateNoteWithText();
-})(this);
+testCreateNoteWithText();
