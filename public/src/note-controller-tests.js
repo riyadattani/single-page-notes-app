10a3c6cb808900db0fba5d37597575bb640696d6
Arthur Ashman <arthurashman@gmail.com>
2019-04-16 18:04:20 +0100

    Update note controller test
    Use mock functionsin test to isolate function under test

--- public/src/note-controller-tests.js
+++ public/src/note-controller-tests.js
@@ -6,11 +6,27 @@ function testThingIsNoteControllerObject() {
 testThingIsNoteControllerObject();
 
 function testControllerRendersNotesList() {
-  var noteList = new NoteList
-  noteList.add("test note")
+  
+  function NoteDouble(text) {
+    this.text = text
+  };
+  var note = new NoteDouble("test note")
+
+  function NoteListDouble() {};
+  NoteListDouble.prototype = {
+    add: function(notetext) {},
+    returnList: function() { return [note]}
+  }
+ 
+ 
+  var noteList = new NoteListDouble
+  noteList.add(note.text)
 
   var noteController = new NoteController(noteList)
   var newHtml = noteController.renderHtml()
   assert.isTrue(newHtml === "<ul><li>test note</li></ul>")
 };
 testControllerRendersNotesList();
+
+// THIS PRINTS TO THE PAGE WHEN RUN
+// Need to double 'document' and give paramenter ot innerhtml to fake renderHtml and not show on screen!!
